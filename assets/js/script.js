d7b8779aaca5e66ebfa097d6f3deea951a09140e
Lucas Cavalcanti de Araujo <lcavalcantidearaujo@gmail.com>
2022-05-10 12:03:25 -0300

    Arquivos adicionados

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+(async function () {
+    document.querySelector(".filmes").innerHTML = "carregando...";
+    let req = await fetch("https://api.b7web.com.br/cinema/");
+    let json = await req.json();
+    console.log(json);
+    montarBlog(json);
+ 
+  function montarBlog(lista) {
+    let html = "";
+
+    for (let i in lista) {
+      html +=
+        `<div class="col-md-4  bg-light p-3"><div class="filme"><img class="img-thumbnail"src="${lista[i].avatar}"/><h5 class="mb-3 text-warning ">${lista[i].titulo}</h5></div></div>`;
+    }
+    document.querySelector(".filmes").innerHTML = html;
+  }
+})();
+
